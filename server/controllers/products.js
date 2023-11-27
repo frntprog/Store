ec6720adbe85fabd9a422d2ef25d497f5cccc6b8
frntprog <frntprog@gmail.com>
2023-11-27 16:03:47 +0200

    basic setup, db, libs, mock data

---
+++ server/controllers/products.js
@@ -0,0 +1,16 @@
+const Product = require("../models/product");
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({ featured: true });
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+const getAllProducts = async (req, res) => {
+  console.log(req.query);
+  res.status(200).json({ msg: "products  route" });
+};
+
+module.exports = {
+  getAllProductsStatic,
+  getAllProducts,
+};
